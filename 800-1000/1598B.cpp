8bc601921141da79d2ddcf8d06ea4b45f5f3b880
Shiladitya Bose <shiladityab24@yahoo.com>
2021-10-25 12:30:43 +0530

    Added folders w.r.t. their ratings

---
+++ 800-1000/1598B.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+using ll = long long;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+    int n;
+    cin>>n;
+    bool ans= false;
+    vector<vector<int>> a(n,vector<int>(5));
+    for(int i=0;i<n;++i){
+        for(int j=0;j<5;++j){
+            cin>>a[i][j];
+        }
+    }
+    if(n & 1){
+        ans = false;
+    }
+    for(int x = 0; x< 5;++x){
+        for(int y = x+1; y<5;++y){
+            int x_cnt = 0,y_cnt = 0,xy_cnt = 0;
+            for(int i = 0;i< n;++i){
+                if(a[i][x] && a[i][y])
+                    xy_cnt++;
+                else if(a[i][x])
+                    x_cnt++;
+                else if(a[i][y])
+                    y_cnt++;
+                else
+                    break;
+                
+            }
+            if(x_cnt +y_cnt+ xy_cnt == n && x_cnt <= n/2 && y_cnt <= n/2){
+                ans = true;
+            }
+        }
+    }
+    if(ans)
+        cout<<"YES"<<'\n';
+    else
+        cout<<"NO"<<'\n';
+    }
+    return 0;
+}
